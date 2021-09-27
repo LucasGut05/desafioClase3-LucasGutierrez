ea2c7317c35d3b51c7811bdb76b69fde5cc3e0e5
LucasGut05 <70763377+LucasGut05@users.noreply.github.com>
2021-09-27 20:49:58 -0300

    subida de archivo

---
+++ MiArchivo.js
@@ -0,0 +1,16 @@
+let valor = Number(
+  prompt("ingrese un valor para la cuenta regresiva o 0 para terminar: ")
+);
+
+while (valor!=0) {
+  
+  
+
+for (let i = valor; i >= 0; i--) {
+  alert(i);
+}
+valor = Number(
+  prompt("ingrese un valor para la cuenta regresiva o 0 para terminar: ")
+);
+
+}
